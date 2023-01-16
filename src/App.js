ae4292a231c16ac5d08e6f44306d3a9845ad502c
Adele Kufour <adele.kufour@news.co.uk>
2023-01-16 18:23:58 +0000

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import { Container } from "./style.js";
+import { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const [feed, setFeed] = useState();
+	const [feedArray, setFeedArray] = useState([]);
+
+	useEffect(() => {
+		console.log(feedArray);
+	});
+	return (
+		<Container>
+			<header>
+				<h1>unintitled</h1>
+			</header>
+
+			<main>
+				<section>
+					{feedArray.map((feedItem, i) => (
+						<p key={`${i}-feedItem`}>{feedItem}</p>
+					))}
+				</section>
+				<textarea
+					onBlur={(e) => {
+						const newValue = e.target.value;
+
+						setFeedArray((feedArray) => [...feedArray, newValue]);
+					}}
+				></textarea>
+				<input type="submit" value="Post" onClick={() => {}}></input>
+			</main>
+		</Container>
+	);
 }
 
 export default App;
